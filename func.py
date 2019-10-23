45ee8e1105bc66fb1503e2e74657cd62c215f16c
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-23 09:00:18 +0300

    Fix Relax method

--- func.py
+++ func.py
@@ -1,10 +1,13 @@
 import numpy as np
 def Relax(A,b,w,E):
-    x = np.zeros_like(b)
-    it_count = 0
+    maxA = 0
+    for i in range(0, A.shape[0]):
+        if A[i, i] > maxA:
+            maxA = A[i, i]
+    x = b / maxA
+    it_count = 1
     while (True):
-        print("Итерация ", it_count, " решение на данной итерации:", x)
-        x_new = np.zeros_like(x)
+        x_new = b/maxA
         for i in range(A.shape[0]):
             sigma = 0
             for j in range(A.shape[1]):
@@ -12,13 +15,15 @@ def Relax(A,b,w,E):
                     sigma += A[i][j] * x[j]
             x_new[i] = (1 - w) * x[i] + (w / A[i][i]) * (b[i] - sigma)
 
-        exit = False
+        iter = 0
         for n in range(0, A.shape[0]):
             if abs(x_new[n] - x[n]) <= E:
-                exit = True
-        if exit == True:
-            break
+                iter += 1
+
         x = x_new
+        print("Итерация ", it_count, " решение на данной итерации:", x)
+        if iter == 3:
+            break
         it_count += 1
 
     print()
