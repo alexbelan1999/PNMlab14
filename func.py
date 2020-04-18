0ede42023a495c8ca3e707d85f0b7f7601bc445c
alexbelan1999 <alexbelan1999@gmail.com>
2020-04-18 17:17:07 +0300

    Fix

--- func.py
+++ func.py
@@ -1,5 +1,7 @@
 import numpy as np
-def Relax(A,b,w,E):
+
+
+def Relax(A, b, w, E):
     maxA = 0
     for i in range(0, A.shape[0]):
         if A[i, i] > maxA:
@@ -7,18 +9,18 @@ def Relax(A,b,w,E):
     x = b / maxA
     it_count = 1
     while (True):
-        x_new = b/maxA
-        for i in range(0,A.shape[0]):
-            #print("A[i, :i]: ", A[i, :i])
-            #print("x_new[:i]: ", x_new[:i])
+        x_new = b / maxA
+        for i in range(0, A.shape[0]):
+            # print("A[i, :i]: ", A[i, :i])
+            # print("x_new[:i]: ", x_new[:i])
             s1 = np.dot(A[i, :i], x_new[:i])
-            #print("s1: ", s1)
-            #print("A[i, i + 1:]: ", A[i, i + 1:])
-            #print("x[i + 1:]: ", x[i + 1:])
+            # print("s1: ", s1)
+            # print("A[i, i + 1:]: ", A[i, i + 1:])
+            # print("x[i + 1:]: ", x[i + 1:])
             s2 = np.dot(A[i, i + 1:], x[i + 1:])
-            #print("s2: ", s2)
-            #print("x_new",[i]," = " ,b[i]/ A[i, i]," - ",s1/ A[i, i]," - ",s2/ A[i, i])
-            x_new[i] = (1 - w) * x[i]+(w/A[i,i])*(b[i] - s1 - s2)
+            # print("s2: ", s2)
+            # print("x_new",[i]," = " ,b[i]/ A[i, i]," - ",s1/ A[i, i]," - ",s2/ A[i, i])
+            x_new[i] = (1 - w) * x[i] + (w / A[i, i]) * (b[i] - s1 - s2)
 
         iter = 0
         for n in range(0, A.shape[0]):
@@ -40,6 +42,7 @@ def Relax(A,b,w,E):
     print("Погрешность: ")
     print(error)
     pass
+
 
 def SpeedDescent(A, b, E):
     maxA = 0
@@ -50,14 +53,14 @@ def SpeedDescent(A, b, E):
     it_count = 1
     while (True):
         x_new = b / maxA
-        r = np.dot(A,x)-b
-        #print("r: ",r)
-        #print("(r,r): ",np.dot(r,r))
-        #print("(A*r,r): ", np.dot(np.dot(A,r),r))
-        t = np.dot(r,r)/np.dot(np.dot(A,r),r)
-        #print("t: ",t)
-        #print("x_new = ", x ," - ",t*r)
-        x_new = x - t*r
+        r = np.dot(A, x) - b
+        # print("r: ",r)
+        # print("(r,r): ",np.dot(r,r))
+        # print("(A*r,r): ", np.dot(np.dot(A,r),r))
+        t = np.dot(r, r) / np.dot(np.dot(A, r), r)
+        # print("t: ",t)
+        # print("x_new = ", x ," - ",t*r)
+        x_new = x - t * r
         iter = 0
         for n in range(0, A.shape[0]):
             if abs(x_new[n] - x[n]) <= E:
@@ -78,4 +81,3 @@ def SpeedDescent(A, b, E):
     print("Погрешность: ")
     print(error)
     pass
-
--- lab14.py
+++ lab14.py
@@ -1,8 +1,9 @@
 import numpy as np
+
 import func
 
 var = 0
-while var not in [1,2]:
+while var not in [1, 2]:
     var = int(input("Выберете метод 1 - релаксации, 2 - скорейшего спуска: "))
 
 E = -1
@@ -10,14 +11,14 @@ while E < 0:
     E = float(input("Введите погрешность: "))
 
 w = -1
-if var ==1:
+if var == 1:
     while w < 0 or w > 2:
         w = float(input("Введите w: "))
-#A = np.array([[10, 1, 1],[2, 10, 1],[2, 2, 10]])
-A1 = np.array([[6.25, -1, 0.5],[-1, 5, 2.12],[0.5, 2.12, 3.6]])
-A2 = np.array([[2.65, -1.27, 0.18],[-1.27, 2.73, -0.46],[0.18, -0.46, 2.16]])
+# A = np.array([[10, 1, 1],[2, 10, 1],[2, 2, 10]])
+A1 = np.array([[6.25, -1, 0.5], [-1, 5, 2.12], [0.5, 2.12, 3.6]])
+A2 = np.array([[2.65, -1.27, 0.18], [-1.27, 2.73, -0.46], [0.18, -0.46, 2.16]])
 print()
-#b = np.array([12, 13, 14])
+# b = np.array([12, 13, 14])
 b1 = np.array([7.5, -8.68, -0.24])
 b2 = np.array([2.25, 0.93, 1.33])
 
@@ -27,7 +28,7 @@ if var == 1:
         row = ["{} * x{}".format(A1[i, j], j + 1) for j in range(A1.shape[1])]
         print(" + ".join(row), "=", b1[i])
     print()
-    func.Relax(A1,b1,w,E)
+    func.Relax(A2, b2, w, E)
 
 if var == 2:
     print("Система 2: ")
@@ -35,4 +36,4 @@ if var == 2:
         row = ["{} * x{}".format(A2[i, j], j + 1) for j in range(A2.shape[1])]
         print(" + ".join(row), "=", b2[i])
     print()
-    func.SpeedDescent(A2,b2,E)
+    func.SpeedDescent(A2, b2, E)
